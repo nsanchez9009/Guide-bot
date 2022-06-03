f8ad79877130887f0f1a57ae8f623f2b72a14620
Nicolas Sanchez <nsanchez9009@gmail.com>
2022-06-03 00:35:51 -0400

    Created a coinflip command and a function prototype for a voodoo command.

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ bot_token = os.environ['BOT_TOKEN']
 #Creating the bot
 bot = lightbulb.BotApp(
     token=bot_token,
-    default_enabled_guilds=(331608355913203713)
+    default_enabled_guilds=(331608355913203713, 831400778643406848)
 )
 
 #8 ball messages
@@ -46,6 +46,8 @@ eightBallMessages = [
     "Concentrate and ask again"
 ]
 
+#Coin
+coin = ["heads", "tails"]
 
 
 #If a message has been sent into the server
@@ -62,11 +64,28 @@ async def pingPong(ctx): #every function requires a context variable
 
 @bot.command
 @lightbulb.option("question", "the question that will soon be answered")
-@lightbulb.command("8ball", "Will send a random 8 ball message")
+@lightbulb.command("8ball", "Will use psychic powers to answer your question")
 @lightbulb.implements(lightbulb.SlashCommand)
 async def eightBall(ctx):
     await ctx.respond(f"Question: {ctx.options.question}\nThe 8ball says...{random.choice(eightBallMessages)}")
 
+#Coinflip
+@bot.command
+@lightbulb.command("coin", "coinflip")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def coinFlip(ctx):
+    await ctx.respond(random.choice(coin))
+
+@bot.command
+@lightbulb.command("voodoo", "When you are ready to challenge the keeper of the underworld, you will have to make a living sacrifice. Everything you need for it can be found in the underworld.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def vooDoo(ctx):
+    #get the bots nickname. Get the numbers in the bots nickname.
+    #store them and add 1 to the number
+    #change the nickname with bot.rest.edit_my_user(username="new kewl username")
+    #bot.get_me().username
+    #Guild.get_my_member().nickname
+    
 
 
 
